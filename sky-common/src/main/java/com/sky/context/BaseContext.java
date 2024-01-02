633b3e68424b0c7006c82aa801fe12e661e79fca
casey <21181214451@stu.xidian.edu.cn>
2024-01-02 20:41:18 +0800

    新增员工功能

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -1,5 +1,8 @@
 package com.sky.context;
 
+//使用ThreadLocal的工具类
+//ThreadLocal可以保存线程的局部变量
+//因为一次请求Tomcat会启动一个线程，所以可以在解析JWT令牌的时候保存用户id便于service层使用
 public class BaseContext {
 
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
