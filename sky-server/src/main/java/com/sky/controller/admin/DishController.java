12927459e25fa6d72a817212c52bbd60c45f3bd4
casey <21181214451@stu.xidian.edu.cn>
2024-01-24 10:56:07 +0800

    缓存菜品业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,10 +11,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.HTML;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -24,6 +26,9 @@ public class DishController {
 
     @Autowired
     private DishService dishService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 新增菜品
@@ -35,6 +40,11 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO) {
         log.info("新增菜品：{}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
+
+        //新增菜品只影响当前分类id的缓存
+        String key = "dish_" + dishDTO.getCategoryId();
+        cleanCache(key);
+
         return Result.success();
     }
 
@@ -61,6 +71,11 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids) {
         log.info("菜品批量删除，{}", ids);
         dishService.deleteBatch(ids);
+
+        //批量删除操作中多个菜品可能属于同一个分类，也可能属于不同的分类
+        //要是一个个去查数据库太繁琐，直接将所有分类的缓存都删掉（菜品批量删除的操作本来就用的少）
+        cleanCache("dish_*");
+
         return Result.success();
     }
 
@@ -87,6 +102,11 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO) {
         log.info("修改菜品，{}", dishDTO);
         dishService.updateWithFlavor(dishDTO);
+
+        //修改菜品需要分情况讨论，可能当前菜品的分类id也被修改了,需要查询数据库
+        //因此可能需要删除一个缓存可能需要删除两个，为了简化操作，直接删除所有缓存
+        cleanCache("dish_*");
+
         return Result.success();
     }
 
@@ -114,6 +134,20 @@ public class DishController {
     public Result startOrStop(@PathVariable Integer status, Long id) {
         log.info("菜品起售停售, status: {} id: {}", status, id);
         dishService.startOrStop(status,id);
+
+        //需要根据菜品id查询分类id，但这样涉及到了数据库的查询操作得不偿失
+        //直接删除所有缓存
+        cleanCache("dish_*");
+
         return Result.success();
+    }
+
+    /**
+     * 清理缓存数据
+     * @param pattern
+     */
+    private void cleanCache(String pattern) {
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
     }
 }
